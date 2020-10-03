309d410eb078787be5dab37a06535193a22a988f
Milan Vaško <m.vasko2@gmail.com>
2020-10-03 19:22:08 +0200

    Fix warnings

--- ingame_ui/ingame_ui.gd
+++ ingame_ui/ingame_ui.gd
@@ -36,10 +36,12 @@ func on_elapsed_time_changed(new_elapsed_time: float) -> void:
 		current_time_node.text = format_time(new_elapsed_time)
 
 func _on_play_again_pressed():
-	get_tree().reload_current_scene()
+	var err = get_tree().reload_current_scene()
+	assert(err == OK)
 
 func _on_main_menu_pressed():
-	get_tree().change_scene("res://main_menu/main_menu.tscn")
+	var err = get_tree().change_scene("res://main_menu/main_menu.tscn")
+	assert(err == OK)
 
 func format_time(time: float) -> String:
 	var seconds = int(time)
