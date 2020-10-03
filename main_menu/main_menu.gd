9144b4b5e6b7960a255903ff6cee6eb5383eb6e0
Milan Vaško <m.vasko2@gmail.com>
2020-10-03 19:45:31 +0200

    Add play menu

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -1,5 +1,5 @@
 extends Control
 
 func _on_play_pressed():
-	var err = get_tree().change_scene("res://main.tscn")
+	var err = get_tree().change_scene("res://main_menu/play_menu/play_menu.tscn")
 	assert(err == OK)
---
+++ main_menu/play_menu/play_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _on_race_with_time_pressed():
+	var err = get_tree().change_scene("res://main.tscn")
+	assert(err == OK)
+
+func _on_race_with_friends_pressed():
+	print("TODO")
+
+func _on_back_pressed():
+	var err = get_tree().change_scene("res://main_menu/main_menu.tscn")
+	assert(err == OK)
