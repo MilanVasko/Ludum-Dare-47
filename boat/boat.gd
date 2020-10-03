d56900093fc61bb1fb149d96aeb8e3c7b6e22b98
Milan Vaško <m.vasko2@gmail.com>
2020-10-03 11:51:32 +0200

    Initial commit

---
+++ boat/boat.gd
@@ -0,0 +1,21 @@
+extends RigidBody2D
+
+onready var left_paddle = $LeftPaddle
+onready var right_paddle = $RightPaddle
+var impulse = 1000
+
+func _physics_process(delta):
+	var paddle = null
+	if Input.is_action_just_pressed("right"):
+		paddle = right_paddle
+	elif Input.is_action_just_pressed("left"):
+		paddle = left_paddle
+
+	if paddle != null:
+		paddle.swing()
+		self.apply_impulse_local(paddle.position, Vector2.UP * impulse)
+
+func apply_impulse_local(force: Vector2, pos: Vector2):
+	var pos_local = self.transform.basis_xform(pos)
+	var force_local = self.transform.basis_xform(force)
+	self.apply_impulse(force_local, pos_local)
---
+++ boat/paddle/paddle.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+onready var rotator = $Rotator
+var is_rotating = false
+var rotation_multiplier = -1.0
+const ROTATION_SPEED = PI * 5
+const MAX_ROTATION = 0
+const MIN_ROTATION = -PI
+
+func swing():
+	is_rotating = true
+	rotator.rotation = MAX_ROTATION
+	rotation_multiplier = -1.0
+
+func _process(delta):
+	if !is_rotating:
+		return
+	rotator.rotation += ROTATION_SPEED * delta * rotation_multiplier
+	if rotator.rotation < MIN_ROTATION:
+		rotator.rotation = MIN_ROTATION
+		rotation_multiplier = 1.0
+	elif rotator.rotation > MAX_ROTATION:
+		rotator.rotation = MAX_ROTATION
+		is_rotating = false
