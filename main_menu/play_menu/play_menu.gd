b2a2c5f671d35b9917e7bcff63d31a3a538bc644
Milan Vaško <m.vasko2@gmail.com>
2020-10-04 12:02:43 +0200

    Add Serpentines map

--- main_menu/play_menu/play_menu.gd
+++ main_menu/play_menu/play_menu.gd
@@ -4,6 +4,10 @@ func _on_circle_map_pressed():
 	Settings.last_selected_map = "circle"
 	go_to_rounds_menu()
 
+func _on_serpentines_map_pressed():
+	Settings.last_selected_map = "serpentines"
+	go_to_rounds_menu()
+
 func go_to_rounds_menu():
 	var err = get_tree().change_scene("res://main_menu/play_menu/rounds_menu/rounds_menu.tscn")
 	assert(err == OK)
