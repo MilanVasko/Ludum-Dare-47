057463a8d2d1b54b4078cf66a67f0c649c77f588
Milan Vaško <m.vasko2@gmail.com>
2020-10-04 18:29:33 +0200

    Refactor play menu a bit

--- main_menu/play_menu/play_menu.gd
+++ main_menu/play_menu/play_menu.gd
@@ -5,19 +5,8 @@ onready var rounds_node = $Container/Rounds/Value
 func _ready():
 	rounds_node.value = Settings.last_number_of_rounds
 
-func _on_circle_map_pressed():
-	Settings.last_selected_map = "circle"
-	go_to_game_mode_menu()
-
-func _on_serpentines_map_pressed():
-	Settings.last_selected_map = "serpentines"
-	go_to_game_mode_menu()
-
-func _on_square_map_pressed():
-	Settings.last_selected_map = "square"
-	go_to_game_mode_menu()
-
-func go_to_game_mode_menu():
+func _on_map_pressed(map_name: String) -> void:
+	Settings.last_selected_map = map_name
 	Settings.last_number_of_rounds = int(rounds_node.value)
 	var err = get_tree().change_scene("res://main_menu/play_menu/game_mode_menu/game_mode_menu.tscn")
 	assert(err == OK)
